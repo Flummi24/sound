c123d7f0003578c2a3a4c7e249b268029264cbdd
Flummi24 <emil.schultz@gyra.hamburg.de>
2025-01-16 18:49:08 +0100

    Update hintergrund.bat

--- hintergrund.bat
+++ hintergrund.bat
@@ -1,7 +1,5 @@
 @echo off
 :Loop
-powershell -Command "$player = New-Object System.Media.SoundPlayer; $player.SoundLocation = 'C:\users\%username%\AD\gas.wav'; $player.PlaySync()"
 powershell -Command "$player = New-Object System.Media.SoundPlayer; $player.SoundLocation = 'C:\users\%username%\AD\kompa.wav'; $player.PlaySync()"
-powershell -Command "$player = New-Object System.Media.SoundPlayer; $player.SoundLocation = 'C:\users\%username%\AD\rick.wav'; $player.PlaySync()"
 goto Loop
 shutdown -s -c "Fehler von DJ.exe"
