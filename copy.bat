3e70f25209a91ae7ddacf190820c84b8c2cb904f
Flummi24 <emil.schultz@gyra.hamburg.de>
2025-01-16 20:01:03 +0100

    Update copy.bat

--- copy.bat
+++ copy.bat
@@ -1,2 +1,4 @@
 @echo off
 copy C:\users\%username%\Downloads\gas.wav C:\users\%username%\AD
+copy C:\users\%username%\Downloads\kompa.wav C:\users\%username%\AD
+copy C:\users\%username%\Downloads\rick.wav C:\users\%username%\AD
