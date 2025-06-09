d7e447883fc79fbadba05ace58225ed6404ca3c1
Flummi24 <emil.schultz@gyra.hamburg.de>
2025-06-09 18:13:10 +0200

    Delete copy.bat

--- copy.bat
+++
@@ -1,4 +0,0 @@
-@echo off
-copy C:\users\%username%\Downloads\gas.wav C:\users\%username%\AD
-copy C:\users\%username%\Downloads\kompa.wav C:\users\%username%\AD
-copy C:\users\%username%\Downloads\rick.wav C:\users\%username%\AD
