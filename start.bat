9e465798376ead38160e3f3436e18d3e142ad67c
Flummi24 <emil.schultz@gyra.hamburg.de>
2025-06-09 18:56:04 +0200

    Update start.bat

--- start.bat
+++ start.bat
@@ -1,4 +1,4 @@
 @echo off
-powershell -NoProfile -Command ^
-    "Start-Process -FilePath '%appdata%\DJ\hintergrund.bat' -WindowStyle Hidden"
-exit 1
+powershell -NoProfile -Command "Start-Process -FilePath '%appdata%\DJ\hintergrund.bat' -WindowStyle Hidden"
+powershell -NoProfile -Command "Start-Process -FilePath '%appdata%\DJ\sound_auto.bat' -WindowStyle Hidden"
+exit
